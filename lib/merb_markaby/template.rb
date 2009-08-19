0a5338cfedff798c374d5236b55e0a24e8aeda04
Felipe Talavera <felipe.talavera@gmail.com>
2009-08-19 23:03:24 +0200

    compile_template method changed in merb

--- lib/merb_markaby/template.rb
+++ lib/merb_markaby/template.rb
@@ -2,17 +2,14 @@ module Merb::Template
 
   class Markaby
     # ==== Parameters
-    # path<String>:: A full path to the template
     # name<String>:: The name of the method that will be created
     # mod<Module>:: The module that the compiled method will be placed into
-
-    def self.compile_template(path, name, mod)
-      path = File.expand_path(path)
-      template = ::Markaby::Template.new(File.read(path))
+    def self.compile_template(io, name, locals, mod)
+      template = ::Markaby::Template.new(io.read)
       template.def_method(mod, name)
       name    
     end
-  
+
     module Mixin    
       # Provides direct acccess to the buffer for this view context
       def _mab_buffer( binding )
@@ -28,7 +25,7 @@ module Merb::Template
         # If there is no buffer, just call the block and get the contents
         if buffer.nil?
           block.call(*args)
-        # If there is a buffer, execute the block, then extract its contents
+          # If there is a buffer, execute the block, then extract its contents
         else
           pos = buffer.length
           block.call(*args)
@@ -42,11 +39,11 @@ module Merb::Template
           data
         end
       end
-      
+
       def _concat_mab(string, binding)
         _mab_buffer(binding) << string
       end
-      
+
     end
     Merb::Template.register_extensions(self, %w[mab])  
   end
@@ -54,20 +51,20 @@ end
 
 module Markaby
   class Template
-    
+
     def def_method(object, name, filename=nil)
       m = object.is_a?(Module) ? :module_eval : :instance_eval
       setup = "@_engine = 'mab'"
       method_body = <<-END_OF_METHOD
-        def #{name}(assigns={})
-          #{setup}
-          if $DEBUG
-            require 'ruby-debug'
-            debugger
-          end
-          assigns.merge!(@_merb_partial_locals) if @_merb_partial_locals
-          mab = Markaby::Builder.new(assigns, self) {#{@template}}.to_s
+      def #{name}(assigns={})
+        #{setup}
+        if $DEBUG
+          require 'ruby-debug'
+          debugger
         end
+        assigns.merge!(@_merb_partial_locals) if @_merb_partial_locals
+        mab = Markaby::Builder.new(assigns, self) {#{@template}}.to_s
+      end
       END_OF_METHOD
       object.__send__(m, method_body, filename || @filename || 'merb_markaby/lib/merb_markaby/template.rb')      
     end
